bf00d5830d3dadca7a1dea3296a5ec20cd78c124
LJ Skadi <lj.skadi@gmail.com>
2018-09-11 11:33:02 +0200

    LJ - Catalog Display

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with:   /\.(gif|jpg|png)\Z/i,
     message: 'must be a URL or GIF, JPG or PNG image'
